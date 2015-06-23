79b7d33b1d825ee3ecb2fc9c71aa06afe25521a9
Vetan16 <vetansalgaoncar@gmail.com>
2015-06-23 16:23:03 +0530

    Update hello.java

--- first/src/myfirst/hello.java
+++ first/src/myfirst/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello MCA");
-System.out.println("Laxmikant you are mad");
+System.out.println("shirish you are mad");
 	}
 
 }
